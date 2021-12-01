8a28518f181b80d75d545f43edc42c1fc1f883b3
xeeny <xeeny@wp.pl>
2021-12-01 23:52:59 +0100

    Add secend part of a task.

--- Day1/Main.hs
+++ Day1/Main.hs
@@ -1,11 +1,23 @@
 import Data.List
 
---- 1709 
+-- Part I
 solve :: String -> Int
 solve xs = snd $ foldl fn (head ints, 0) $ ints
     where ints = map read $ lines xs :: [Int]
           fn (last, acc) x | x > last  = (x, acc + 1)
                            | otherwise = (x, acc)
 
+-- Part II
+solve' :: String -> Int
+solve' xs = snd $ foldl fn (head ints, 0) $ ints
+    where ints = cal_sums $ map read $ lines xs :: [Int]
+          fn (last, acc) x | x > last  = (x, acc + 1)
+                           | otherwise = (x, acc)
+          cal_sums (x1:x2:x3:xs) = 
+              (x1 + x2 + x3) : if length xs > 0
+                             then cal_sums (x2:x3:xs)
+                             else []
+
+
 main :: IO ()
-main = readFile "input.txt" >>= print . solve
+main = readFile "input.txt" >>= print . solve'
