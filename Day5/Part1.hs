b5bdc7aabd8ab86f0352f9502bbae4e0f51937c9
xeeny <xeeny@wp.pl>
2021-12-05 21:15:38 +0100

    solve day 5

---
+++ Day5/Part1.hs
@@ -0,0 +1,28 @@
+import Data.List.Extra
+
+parse :: String -> [((Int, Int), (Int, Int))]
+parse = map (toPiar . map (toPiar . map read . splitOn ",") . splitOn " -> ") . lines
+    where toPair [x,y] = (x,y)
+
+pathToPoint :: ((Int, Int), (Int, Int)) -> [(Int, Int)]
+pathToPoint ((ax, ay), (bx, by)) 
+        | ax == bx  = [(ax, y) | y <- [top ..bot]]
+        | ay == by  = [(x, ay) | x <- [left..right]]
+        | otherwise = []
+    where top   = min ay by
+          bot   = max ay by
+          left  = min ax bx
+          right = max ax bx
+
+counts :: (Eq a, Ord a) => [a] -> [(a,Int)]
+counts = foldl counts' [] . sort
+    where counts' :: Eq a => [(a, Int)] -> a -> [(a, Int)]
+          counts' []         x             = [(x, 1)]
+          counts' ((a,c):as) x | a == x    = (a,c+1):as
+                               | otherwise = (x,1):(a,c):as
+
+solve :: String -> Int
+solve = length . filter (>1) . map snd . counts . concat . map pathToPoint . parse
+
+main :: IO ()
+main = readFile "input.txt" >>= print . solve
